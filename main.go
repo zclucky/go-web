9e57c079ea52923c640c9fed2b224d1e9a12e6b8
zc <myxxqy@gmail.com>
2018-10-13 16:23:30 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func hello(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("hello, this is version 1!"))
+	request.ParseForm()
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	log.Println("String server ... v1")
+	err := http.ListenAndServe(":9090", nil)
+	if err == nil{
+		log.Fatal("Listion and serve: ",err)
+	}
+}
+
+
